dbd85bde9e965c94e38b52937e43049f02b5ef8d
Michał Szeler <michsze007@student.polsl.pl>
2020-04-23 15:07:04 +0200

    cleanup commit

--- Simple Chatty Bot/Problems/Cats are robots/src/Main.java
+++
@@ -1,5 +0,0 @@
-class Main {
-    public static void main(String[] args) {
-        System.out.println("Cats are robots!");
-    }
-}
--- Simple Chatty Bot/Problems/Replacing 'a' with 'b'/src/Main.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-class Main {
-    public static void main(String[] args) {
-        // put your code here
-        String value = (new Scanner(System.in)).next();
-        System.out.println(value.replace("a","b"));
-    }
-}
